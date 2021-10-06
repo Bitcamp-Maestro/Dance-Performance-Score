acb342cd20a9d4dbf87d1ae0f73d61596e649ee9
Baehyeok <85778314+Baehyeok@users.noreply.github.com>
2021-10-06 12:58:52 +0900

    Fix data preprocessing
    Fix dtw.py
    add mix_video.py

--- module/utils/dtw.py
+++ module/utils/dtw.py
@@ -3,13 +3,10 @@ import librosa
 import librosa.core
 import librosa.display
 
-from audio import audio, melody
-
-
 # Load Audio Recordings 
 # First, we loaded audio from the professional dancer which is target video 
 
-def Dtw(x1_chroma,x2_chroma):
+def Dtw(x1_chroma,x2_chroma, sorted):
     """
     Computes Dynamic Time Wariping DTW) of two sequences.
 
@@ -17,7 +14,12 @@ def Dtw(x1_chroma,x2_chroma):
     hop_size = 2205
 
     D, wp = librosa.sequence.dtw(x1_chroma, x2_chroma, subseq=True)
-    wp_s = np.asarray(wp) * hop_size / sorted
-    start_point = wp_s[:, 0][-1]
-    end_point = wp_s[:, 0][0] - start_point
-    return start_point, end_point
+    wp_s = np.asarray(wp) * int(hop_size) / sorted
+    start_point = wp_s[:, 1][-1]
+    # end_point = wp_s[:, 1][0] - start_point
+    end_point = wp_s[:, 1][0]
+    result = {
+        "start" : start_point,
+        "end" : end_point
+    }
+    return result
