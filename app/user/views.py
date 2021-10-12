24bff3ac6358936d9e2db52b21c909d478fa2f4c
dleego <thisisdylee@gmail.com>
2021-10-12 19:00:58 +0900

    Update views.py

--- app/user/views.py
+++ app/user/views.py
@@ -54,34 +54,6 @@ def register(request):
         return render(request, 'register.html')
 
 
-
-# # Create your views here.
-# def register(request):   #회원가입 페이지를 보여주기 위한 함수
-#     if request.method == "GET":
-#         return render(request, 'register.html')
-
-#     elif request.method == "POST":
-#         username = request.POST.get('username',None)   #딕셔너리형태
-#         email = request.POST.get('email', None)
-#         password = request.POST.get('password',None)
-#         re_password = request.POST.get('re_password',None)
-#         res_data = {} 
-#         if not (username and email and password and re_password) :
-#             res_data['error'] = "모든 값을 입력해야 합니다."
-#         elif password != re_password :
-#             # return HttpResponse('비밀번호가 다릅니다.')
-#             res_data['error'] = '비밀번호가 다릅니다.'
-#         elif password == re_password : 
-#             res_data['error'] = None
-#             user = User(username=username, email=email, password=make_password(password))
-#             user.save()
-
-#         if res_data['error'] == None:
-#             return redirect('/user/login')      
-
-#         else:
-#             return render(request, 'register.html', res_data)   
-
 def login(request):
     
 
